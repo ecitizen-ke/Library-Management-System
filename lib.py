682d14f049fa9ad3d5188a8d70f5db7af716ae36
sawasana <faithjebichy@gmail.com>
2024-05-13 11:36:31 +0300

    delete unnecessary files

--- lib.py
+++ lib.py
@@ -1,40 +1,44 @@
 import json
 
-#Represents a book with attributes title, author
-class Book:  
+
+# Represents a book with attributes title, author
+class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
         self.checked_out = False
-        
+
     def __str__(self):
         return f"{self.title} by {self.author}"
 
-#Manages a collection of books with different methods
+
+# Manages a collection of books with different methods
 class Library:
     def __init__(self):
         self.books = []
-        
+
     def add_book(self, title, author):
         book = Book(title, author)
         self.books.append(book)
         print(f"Book '{title}' added successfully.")
-        
+
     def display_books(self):
         if not self.books:
             print("No books available in the library.")
         else:
             for book in self.books:
                 print(book)
-                
+
     def search_book(self, title):
-        found_books = [book for book in self.books if title.lower() in book.title.lower()]
+        found_books = [
+            book for book in self.books if title.lower() in book.title.lower()
+        ]
         if found_books:
             for book in found_books:
                 print(book)
         else:
             print("Book not found")
-            
+
     def check_out_book(self, title):
         for book in self.books:
             if title.lower() == book.title.lower() and not book.checked_out:
@@ -42,7 +46,7 @@ class Library:
                 print(f"Book '{title}' checked out successfully.")
                 return
         print("Book not available for checkout.")
-        
+
     def display_check_out_books(self):
         checked_out_books = [book for book in self.books if book.checked_out]
         if checked_out_books:
@@ -51,28 +55,33 @@ class Library:
                 print(book)
         else:
             print("No books are currently checked out.")
-                
+
     def return_book(self, title):
         for book in self.books:
             if title.lower() == book.title.lower() and book.checked_out:
                 book.checked_out = False
                 print(f"Book '{title} returned successfully.")
                 return
-        print("Book cannot be returned.") 
-        
+        print("Book cannot be returned.")
+
     def save_library_to_file(self, filename):
         filename = "library.json"
         with open(filename, "w") as filename:
-         json.dump([vars(book) for book in self.books], filename) #Saves the library data (book details) to a JSON file 
+            json.dump(
+                [vars(book) for book in self.books], filename
+            )  # Saves the library data (book details) to a JSON file
         print("Library data saved successfully.")
-        
+
     def load_library_from_file(self, filename):
         with open(filename, "r") as file:
-            data = json.load(file) #Saves the library data (book details) to a JSON file 
+            data = json.load(
+                file
+            )  # Saves the library data (book details) to a JSON file
             self.books = [Book(**book_data) for book_data in data]
         print("Library data loaded successfully.")
-        
- # Contains a loop for displaying a menu and handling user input
+
+
+# Contains a loop for displaying a menu and handling user input
 def main():
     library = Library()
     while True:
@@ -87,13 +96,13 @@ def main():
         print("8. Load library data")
         print("9. Delete book")
         print("10. Exit Library")
-        
+
         choice = input("Enter your choice:")
-    
-        if choice == '1':
-            title = input("Enter title of the book: ")        
-            author = input("Enter author of the book: ")    
-            library.add_book(title, author)    
+
+        if choice == "1":
+            title = input("Enter title of the book: ")
+            author = input("Enter author of the book: ")
+            library.add_book(title, author)
         elif choice == "2":
             library.display_books()
         elif choice == "3":
@@ -117,8 +126,7 @@ def main():
             print("Exit from the library")
         else:
             print("Invalid choice.Please enter a number from 1 to 9")
-            
+
+
 if __name__ == "__main__":
     main()
-    
-                
